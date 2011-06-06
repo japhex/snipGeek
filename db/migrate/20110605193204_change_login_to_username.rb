24fed0c9efd04e2803c840b72832155d73e9fb8c
Jamie Lawrence <j@japhex.net>
2011-06-06 19:57:21 +0100

    Deployment to Heroku again

--- db/migrate/20110605193204_change_login_to_username.rb
+++ db/migrate/20110605193204_change_login_to_username.rb
@@ -1,11 +1,11 @@
 class ChangeLoginToUsername < ActiveRecord::Migration
   def self.up
-    remove_column :users, :login, :string
+    remove_column :users, :login
     add_column :users, :username, :string
   end
 
   def self.down
-    remove_column :users, :username, :string
+    remove_column :users, :username
     add_column :users, :login, :string
   end
 end
